c2ae6d11ee069275071f126fb18c73a11bc88e1f
DPChanny <kyoungchan0516@gmail.com>
2025-04-30 14:01:40 +0900

    only sample

--- unity/Assets/Settings/Config.cs
+++ unity/Assets/Settings/Config.cs
@@ -2,8 +2,7 @@
 {
     public static class Config
     {
-        public const string DataPath = @"C:\Dev\Repos\ARMap\data";
-        public const string FfmpegPath = @"C:\Dev\Repos\ARMap\ffmpeg\bin\ffmpeg.exe";
+        public const string DataPath = @"C:\Devs\Repos\ARMap\data";
         public const string SampleFile = "sample.mp4";
     }
 }
